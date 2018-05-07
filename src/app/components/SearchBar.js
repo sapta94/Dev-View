672e93e86f5440184269843cfefd4f0ece68d673
Saptarshi Dey <dey7.kol@gmail.com>
2018-05-07 22:45:41 +0530

    Added search abr component

---
+++ src/app/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM,{render} from "react-dom";
+
+
+class SearchBar extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="searchBar">
+                
+            </div>
+        )
+    }
+}
