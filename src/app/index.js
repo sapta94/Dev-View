e9a468d91c5e2aa6258aa4bd6d12f5aff3d62f3d
Saptarshi Dey <dey7.kol@gmail.com>
2018-05-09 23:00:45 +0530

    rendering searchbar component

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM,{render} from "react-dom";
 import Header from './components/Header'
+import SearchBar from './components/SearchBar'
 
 class App extends React.Component{
     render(){
         return (
             <div>
                 <Header imgUrl='https://media-public.fcbarcelona.com/20157/74594198/910692/1.2/910692.jpg'/>
+                <SearchBar/>
             </div>
         )
     }
