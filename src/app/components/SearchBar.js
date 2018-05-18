f8ace382fdded8da9b2423f8ed24baa08f82b21f
Saptarshi Dey <dey7.kol@gmail.com>
2018-05-18 22:51:25 +0530

    added search fields

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -9,8 +9,34 @@ class SearchBar extends React.Component{
 
     render(){
         return(
-            <div className="searchHead">
-                <p>-Search Here-</p>
+            <div>
+                <div className="searchHead">
+                    <p>-Search Here-</p>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="usr">Name:</label>
+                    <input type="text" class="form-control" id="usr"/>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="loc">Location:</label>
+                    <input type="text" class="form-control" id="loc"/>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="repo">Repositories:</label>
+                    <input type="text" class="form-control" id="repo"/>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="topic">Topics:</label>
+                    <input type="text" class="form-control" id="topic"/>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="topic">Topics:</label>
+                    <input type="text" class="form-control" id="topic"/>
+                </div>
+                <div class="form-group col-md-8">
+                    <label for="commit">Commits:</label>
+                    <input type="text" class="form-control" id="commit"/>
+                </div>
             </div>
         )
     }
