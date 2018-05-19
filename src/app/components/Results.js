c5e423846f3d699df5ae583d83dff54b32e9d8a5
Saptarshi Dey <dey7.kol@gmail.com>
2018-05-19 20:01:52 +0530

    added result component

---
+++ src/app/components/Results.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM,{render} from "react-dom";
+import axios from 'axios'
+
+class Results extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        var data=this.props.result
+
+        return(
+            <div className="row">
+            {
+                data.map(function(item,index){
+                    return(
+                        <div className="card col-md-4" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={item.avatar_url} alt="Card image cap"/>
+                        <div className="card-body">
+                            <p className="card-text"><b>GITHUB ID: </b>{item.login} </p>
+                            <p className="card-text"><b>Score: </b>{item.score} </p>
+                            <a href={item.html_url} target="_blank" className="card-link">View Profile</a>
+                        </div>
+                        </div>
+                    )
+                })
+            }
+
+            </div>
+        )
+    }
+}
+
+export default Results
