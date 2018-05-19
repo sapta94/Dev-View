60a7ef97763fe306ad3bb384b6cc1ce82b88f8a3
Saptarshi Dey <dey7.kol@gmail.com>
2018-05-19 17:46:46 +0530

    adding states and serach button

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -5,9 +5,32 @@ import ReactDOM,{render} from "react-dom";
 class SearchBar extends React.Component{
     constructor(props){
         super(props)
+        this.state={
+            formData:{
+                'name':'',
+                'loc':'',
+                'repo':'',
+                'lang':'',
+                'commit':''
+            }
+        }
+        this.onChange=this.onChange.bind(this)
+    }
+
+    onChange(e){
+        var id=e.target.name;
+        var val=e.target.value;
+        console.log(id+'-'+val)
+        var formData = this.state.formData;
+        formData[id]=val;
+        this.setState({
+            formData:formData
+        })
     }
 
     render(){
+        var data=this.state.formData;
+        var that=this;
         return(
             <div>
                 <div className="searchHead">
@@ -15,30 +38,27 @@ class SearchBar extends React.Component{
                 </div>
                 <div class="row justify-content-md-center">
                 <div class="form-group col-md-8">
-                    <label for="usr">Name:</label>
-                    <input type="text" class="form-control" id="usr"/>
+                    <label for="name">Name:</label>
+                    <input type="text" onChange={(e)=>{that.onChange(e)}} value={data.name} class="form-control" name="name"/>
                 </div>
                 <div class="form-group col-md-8">
                     <label for="loc">Location:</label>
-                    <input type="text" class="form-control" id="loc"/>
+                    <input type="text" onChange={(e)=>{that.onChange(e)}} value={data.loc} class="form-control" name="loc"/>
                 </div>
                 <div class="form-group col-md-8">
                     <label for="repo">Repositories:</label>
-                    <input type="text" class="form-control" id="repo"/>
+                    <input type="text" onChange={(e)=>{that.onChange(e)}} value={data.repo} class="form-control" name="repo"/>
                 </div>
                 <div class="form-group col-md-8">
-                    <label for="topic">Topics:</label>
-                    <input type="text" class="form-control" id="topic"/>
-                </div>
-                <div class="form-group col-md-8">
-                    <label for="topic">Topics:</label>
-                    <input type="text" class="form-control" id="topic"/>
+                    <label for="lang">Language:</label>
+                    <input type="text" onChange={(e)=>{that.onChange(e)}} value={data.lang} class="form-control" name="lang"/>
                 </div>
                 <div class="form-group col-md-8">
                     <label for="commit">Commits:</label>
-                    <input type="text" class="form-control" id="commit"/>
+                    <input type="text" onChange={(e)=>{that.onChange(e)}} value={data.commit} class="form-control" name="commit"/>
                 </div>
                 </div>
+                <center><button type="button" class="btn btn-primary">Search</button></center>
             </div>
         )
     }
