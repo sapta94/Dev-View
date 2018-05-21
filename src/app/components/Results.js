ef44fd4e7997053c46f1ef5709c98d7ab2a05ede
Saptarshi Dey <dey7.kol@gmail.com>
2018-05-21 23:17:24 +0530

    orientaion change and button to view more details

--- src/app/components/Results.js
+++ src/app/components/Results.js
@@ -18,12 +18,13 @@ class Results extends React.Component{
             {
                 data.map(function(item,index){
                     return(
-                        <div className="card col-md-4" style={{width: "12rem"}}>
+                        <div className="card col-md-3" style={{width: "12rem",marginLeft:"70px",marginBottom:"10px"}}>
                         <img className="card-img-top" src={item.avatar_url} alt="Card image cap"/>
                         <div className="card-body">
                             <p className="card-text"><b>GITHUB ID: </b>{item.login} </p>
                             <p className="card-text"><b>Score: </b>{item.score} </p>
                             <a href={item.html_url} target="_blank" className="card-link">View Profile</a>
+                            <a href={item.html_url} target="_blank" className="card-link">More details</a>
                         </div>
                         </div>
                     )
